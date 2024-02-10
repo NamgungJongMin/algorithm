4298258e7aca0fa68e66429c4703ec489b607337
Namgung Jong Mim <jmnamgung@gmail.com>
2024-02-11 03:17:49 +0900

    [Gold IV] Title: 고층 건물, Time: 132 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.js
+++ 백준/Gold/1027. 고층 건물/고층 건물.js
@@ -1,52 +1,47 @@
-/*
-내가 지금 보고 있는 건물 X의 오른쪽으로 몇 개가 보이는지 아는 법
+const fs = require('fs');
 
-일단 가장 가까운 건물 A를 본다.
-X와 A 꼭대기를 잇는 선분의 기울기 prevGradient 를 구한다.
-다음 건물 B을 본다.
-X와 B 꼭대기를 잇는 선분의 기울기 curGradient 를 구한다.
-prevGredient >= curGradient 면 안보인다.
-다음 건물 C를 본다...
->> 점점 기울기가 커져야 함.
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const number = Number(input[0]);
+const heights = input[1].split(' ').map(el => Number(el));
 
-왼쪽으로 몇 개가 보이는지는 반대로 하면 됨.
-*/
-// constants
-const RIGHT = 1;
-const LEFT = -1;
+let answer = 0;
 
-// function
-const look = (curHeight, startIndex, heights, direction) => {
-  let prevGradient = -Infinity;
-  let count = 0;
+if (number === 1) {
+  console.log(0);
+  return;
+}
 
-  for (let i = startIndex + direction; i >= 0 && i < heights.length; i += direction) {
-    let curGradient = (heights[i] - curHeight) / (i - startIndex);
-    curGradient *= direction;
+for (let i = 0; i < number; i++) {
+  let sum = 0;
 
-    if (prevGradient < curGradient) {
-      prevGradient = curGradient;
-      count += 1;
+  if (i < number - 1) {
+    sum++;
+    let prevSlopeR = heights[i + 1] - heights[i];
+
+    for (let j = i + 2; j < number; j++) {
+      const newSlopeR = (heights[j] - heights[i]) / (j - i);
+      if (prevSlopeR < newSlopeR) {
+        sum++;
+        prevSlopeR = newSlopeR;
+      }
     }
   }
 
-  return count;
-};
+  if (i > 0) {
+    sum++;
+    let prevSlopeL = heights[i] - heights[i - 1];
 
-// input
-const inputFile = process.platform === 'linux' ? '/dev/stdin' : './input';
-const [N, ...heights] = require('fs').readFileSync(inputFile).toString().trim()
-  .split(/\s/)
-  .map(Number);
+    for (let j = i - 2; j >= 0; j--) {
+      const newSlopeL = (heights[i] - heights[j]) / (i - j);
 
-// process
-let sol = 0;
-heights.forEach((height, index, array) => {
-  sol = Math.max(
-    look(height, index, array, RIGHT) + look(height, index, array, LEFT),
-    sol,
-  );
-});
+      if (prevSlopeL > newSlopeL) {
+        sum++;
+        prevSlopeL = newSlopeL;
+      }
+    }
+  }
 
-// output
-console.log(sol);
+  if (sum > answer) answer = sum;
+}
+
+console.log(answer);
