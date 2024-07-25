13f0b7857c69471171874d18b85faba5bdbc1b67
namgungjongmin <jmnamgung@gmail.com>
2024-07-26 06:16:20 +0900

    2024.07.26 bruteforce2

--- 문제모음/섹션4/3. !!멘토링.js
+++ 문제모음/섹션4/3. !!멘토링.js
@@ -1,25 +1,48 @@
 // NOTE: 알고리즘 적으로 어떻게 풀지 모르겠다 -> 완전탐색으로 모든 경우의수 찾고 제외하기
 
-function solution(test) {
-  //   let answer = 0;
-  //   m = test.length;
-  //   n = test[0].length;
-  //   for (let i = 1; i <= n; i++) {
-  //     for (let j = 1; j <= n; j++) {
-  //       let cnt = 0;
-  //       for (let k = 0; k < m; k++) {
-  //         let pi = (pj = 0);
-  //         for (let s = 0; s < n; s++) {
-  //           if (test[k][s] === i) pi = s;
-  //           if (test[k][s] === j) pj = s;
-  //         }
-  //         if (pi < pj) cnt++;
-  //       }
-  //       if (cnt === m) answer++;
-  //     }
-  //   }
-  //   return answer;
+// function solution(test) {
+//   //   let answer = 0;
+//   //   m = test.length;
+//   //   n = test[0].length;
+//   //   for (let i = 1; i <= n; i++) {
+//   //     for (let j = 1; j <= n; j++) {
+//   //       let cnt = 0;
+//   //       for (let k = 0; k < m; k++) {
+//   //         let pi = (pj = 0);
+//   //         for (let s = 0; s < n; s++) {
+//   //           if (test[k][s] === i) pi = s;
+//   //           if (test[k][s] === j) pj = s;
+//   //         }
+//   //         if (pi < pj) cnt++;
+//   //       }
+//   //       if (cnt === m) answer++;
+//   //     }
+//   //   }
+//   //   return answer;
 
+//   let answer = 0;
+
+//   for (let i = 1; i <= test[0].length; i++) {
+//     for (let j = 1; j <= test[0].length; j++) {
+//       if (i === j) continue;
+
+//       let isMatched = true;
+
+//       for (let k = 0; k < test.length; k++) {
+//         if (test[k].indexOf(i) > test[k].indexOf(j)) {
+//           isMatched = false;
+//           break;
+//         }
+//       }
+
+//       if (isMatched) answer++;
+//     }
+//   }
+
+//   return answer;
+// }
+
+function solution(test) {
   let answer = 0;
 
   for (let i = 1; i <= test[0].length; i++) {
@@ -27,7 +50,6 @@ function solution(test) {
       if (i === j) continue;
 
       let isMatched = true;
-
       for (let k = 0; k < test.length; k++) {
         if (test[k].indexOf(i) > test[k].indexOf(j)) {
           isMatched = false;
@@ -35,7 +57,7 @@ function solution(test) {
         }
       }
 
-      if (isMatched) answer++;
+      if (isMatched) answer += 1;
     }
   }
 
