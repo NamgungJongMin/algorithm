3c06144806f1201e5b16dadebf29885d16e5633d
namgungjongmin <jmnamgung@gmail.com>
2024-07-28 04:45:36 +0900

    2024.07.28 two pointer

--- 문제모음/섹션5/1. !!두 배열 합치기.js
+++ 문제모음/섹션5/1. !!두 배열 합치기.js
@@ -2,41 +2,72 @@
 // <<투 포인터 알고리즘>>
 
 function solution(arr1, arr2) {
-  //   let answer = [];
-  //   let n = arr1.length;
-  //   let m = arr2.length;
-  //   let p1 = (p2 = 0);
-  //   while (p1 < n && p2 < m) {
-  //     if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
-  //     else answer.push(arr2[p2++]);
-  //   }
-  //   while (p1 < n) answer.push(arr1[p1++]);
-  //   while (p2 < m) answer.push(arr2[p2++]);
-  //   return answer;
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  let p1 = (p2 = 0);
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+    else answer.push(arr2[p2++]);
+  }
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+  return answer;
+}
 
+function solution(arr1, arr2) {
   let answer = [];
   let p1 = 0;
   let p2 = 0;
 
-  while (arr1[p1] !== Number.MAX_SAFE_INTEGER || arr2[p2] !== Number.MAX_SAFE_INTEGER) {
-    if (arr1[p1] <= arr2[p2]) {
-      answer.push(arr1[p1]);
-      if (p1 === arr1.length - 1) {
-        arr1[p1] = Number.MAX_SAFE_INTEGER;
-      } else {
-        p1++;
-      }
-    } else {
-      answer.push(arr2[p2]);
-      if (p2 === arr2.length - 1) {
-        arr2[p2] = Number.MAX_SAFE_INTEGER;
-      } else {
-        p2++;
-      }
-    }
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+    else answer.push(arr2[p2++]);
   }
+  while (p1 < arr1.length) answer.push(arr1[p1++]);
+  while (p2 < arr2.length) answer.push(arr2[p2++]);
+
   return answer;
 }
+
+// NOTE: 잘못된 답
+
+// function solution(arr1, arr2) {
+//   return [...arr1, ...arr2].sort((a, b) => a - b);
+// }
+
+// function solution(arr1, arr2) {
+//   const answer = [];
+//   let pointerA = 0;
+//   let pointerB = 0;
+
+//   while (pointerA < arr1.length || pointerB < arr2.length) {
+//     if (arr1[pointerA] < arr2[pointerB]) {
+//       answer.push(arr1[pointerA]);
+//       pointerA += 1;
+//     } else if (arr1[pointerA] > arr2[pointerB]) {
+//       answer.push(arr2[pointerB]);
+//       pointerB += 1;
+//     } else if (arr1[pointerA] === arr2[pointerB]) {
+//       answer.push(arr1[pointerA]);
+//       pointerA += 1;
+//       answer.push(arr2[pointerB]);
+//       pointerB += 1;
+//     } else {
+//       if (arr1[pointerA]) {
+//         answer.push(arr1[pointerA]);
+//         pointerA += 1;
+//       } else {
+//         answer.push(arr2[pointerB]);
+//         pointerB += 1;
+//       }
+//     }
+
+//     console.log(pointerA, pointerB);
+//   }
+
+//   return answer;
+// }
 
 let a = [1, 3, 5];
 let b = [2, 3, 6, 7, 9];
--- 문제모음/섹션5/2. 공통원소구하기.js
+++ 문제모음/섹션5/2. 공통원소구하기.js
@@ -3,19 +3,43 @@
 // sort는 nlogN / 배열 탐색 메서드의 경우는 n의 복잡도로 반복문 안의 배열 메서드 등으로 해결할 시 n^2의 복잡도로 풀게 된다.
 // 투포인터 알고리즘을 사용하면 n의 복잡도로도 풀이를 할 수있다.
 
+// function solution(arr1, arr2) {
+//   let answer = [];
+//   arr1.sort((a, b) => a - b);
+//   arr2.sort((a, b) => a - b);
+//   let p1 = (p2 = 0);
+//   while (p1 < arr1.length && p2 < arr2.length) {
+//     if (arr1[p1] == arr2[p2]) {
+//       answer.push(arr1[p1++]);
+//       p2++;
+//     } else if (arr1[p1] < arr2[p2]) p1++;
+//     else p2++;
+//   }
+//   return answer;
+// }
+
 function solution(arr1, arr2) {
-  //   let answer = [];
-  //   arr1.sort((a, b) => a - b);
-  //   arr2.sort((a, b) => a - b);
-  //   let p1 = (p2 = 0);
-  //   while (p1 < arr1.length && p2 < arr2.length) {
-  //     if (arr1[p1] == arr2[p2]) {
-  //       answer.push(arr1[p1++]);
-  //       p2++;
-  //     } else if (arr1[p1] < arr2[p2]) p1++;
-  //     else p2++;
-  //   }
-  //   return answer;
+  let answer = [];
+  let p1 = 0;
+  let p2 = 0;
+
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  while (p1 < arr1.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+    } else {
+      p2++;
+    }
+
+    if (p2 === arr2.length) {
+      p1++;
+      p2 = 0;
+    }
+  }
+
+  return answer;
 }
 
 let a = [1, 3, 9, 5, 2];
