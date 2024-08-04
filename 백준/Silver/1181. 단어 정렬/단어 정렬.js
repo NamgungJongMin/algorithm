be38fecfe41d7b6010d6719c79f85804b0978696
Namgung Jong Mim <jmnamgung@gmail.com>
2024-08-04 13:42:43 +0900

    [Silver V] Title: 단어 정렬, Time: 932 ms, Memory: 24052 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
+
+const n = +input[0];
+const words = Array.from(new Set(input.slice(1, input.length)));
+// console.log(n, words);
+
+words.sort((a, b) => {
+  if (a.length === b.length) {
+    if (a > b) return 1;
+    else if (a < b) return -1;
+    else return 0;
+  } else return a.length - b.length;
+});
+
+words.forEach((el) => console.log(el));
