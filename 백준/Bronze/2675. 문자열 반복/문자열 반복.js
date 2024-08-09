8366e2b3d1cf7080e9a448b7094e02787c8d4935
Namgung Jong Mim <jmnamgung@gmail.com>
2024-08-09 21:03:47 +0900

    [Bronze II] Title: 문자열 반복, Time: 88 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const arr = input.map((el) => el.split(" ").map((v) => v.trim()));
+
+arr.forEach((el) => {
+  if (el.length === 1) return;
+  let str = "";
+
+  for (let i = 0; i < el[1].length; i++) {
+    for (let j = 1; j <= +el[0]; j++) {
+      str += el[1][i];
+    }
+  }
+
+  console.log(str);
+});
