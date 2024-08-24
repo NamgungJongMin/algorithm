4e8c94125b4b6ea604e4cdb73c2ffa4bfccfa32f
Namgung Jong Mim <jmnamgung@gmail.com>
2024-08-24 22:17:54 +0900

    [Gold III] Title: 세 용액, Time: 960 ms, Memory: 12456 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const arr = input[1].split(" ").map(Number);
+let answer = Array(3).fill(1000000000);
+
+arr.sort((a, b) => a - b);
+
+const sum = (arr) => arr.reduce((acc, cur) => acc + cur);
+
+for (let i = 0; i < arr.length; i++) {
+  let left = i + 1;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if (Math.abs(sum([arr[i], arr[left], arr[right]])) < Math.abs(sum(answer))) {
+      answer = [arr[i], arr[left], arr[right]];
+    }
+
+    if (arr[i] + arr[left] + arr[right] > 0) {
+      right--;
+    } else if (arr[i] + arr[left] + arr[right] < 0) {
+      left++;
+    } else break;
+  }
+}
+
+console.log(...answer);
