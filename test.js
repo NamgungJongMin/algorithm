43e7169d36abd3c87926c0fc9a3af02b0bd638ef
namgungjongmin <jmnamgung@gmail.com>
2024-08-27 16:35:41 +0900

    update

--- test.js
+++ test.js
@@ -1,28 +1,91 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("test.txt").toString().trim().split("\n");
-const arr = input[1].split(" ").map(Number);
-let answer = Array(3).fill(1000000000);
+const T = +input[0].trim();
+const arr = input
+  .slice(1)
+  .filter((_, idx) => idx % 2 === 1)
+  .map((el) => el.split(" ").map(Number));
 
-arr.sort((a, b) => a - b);
+class MinHeap {
+  constructor() {
+    this.heap = [];
+    this.length = 0;
+  }
 
-const sum = (arr) => arr.reduce((acc, cur) => acc + cur);
+  push(value) {
+    this.heap.push(value);
+    this.length++;
+    this.heapifyUp();
+  }
 
-for (let i = 0; i < arr.length; i++) {
-  let left = i + 1;
-  let right = arr.length - 1;
+  delete() {
+    const root = this.heap[0];
 
-  while (left < right) {
-    if (Math.abs(sum([arr[i], arr[left], arr[right]])) < Math.abs(sum(answer))) {
-      answer = [arr[i], arr[left], arr[right]];
+    if (this.heap.length === 0) return null;
+    else if (this.heap.length === 1) {
+      this.heap = [];
+      this.length = 0;
+    } else {
+      this.heap[0] = this.heap.pop();
+      this.heapifyDown();
+      this.length--;
     }
 
-    if (arr[i] + arr[left] + arr[right] > 0) {
-      right--;
-    } else if (arr[i] + arr[left] + arr[right] < 0) {
-      left++;
-    } else break;
+    return root;
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
+  }
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+  swap(index1, index2) {
+    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+  }
+  heapifyUp() {
+    let cIdx = this.heap.length - 1;
+
+    while (cIdx > 0) {
+      const parentIdx = this.getParentIndex(cIdx);
+
+      if (this.heap[cIdx] < this.heap[parentIdx]) {
+        this.swap(cIdx, parentIdx);
+        cIdx = parentIdx;
+      } else break;
+    }
+  }
+  heapifyDown() {
+    let cIdx = 0;
+
+    while (this.getLeftChildIndex(cIdx) < this.heap.length) {
+      const leftChildIndex = this.getLeftChildIndex(cIdx);
+      const rightChildIndex = this.getRightChildIndex(cIdx);
+      const targetIndex = this.heap[leftChildIndex] > this.heap[rightChildIndex] ? rightChildIndex : leftChildIndex;
+      // 자식노드가 하나만 있을 때에는 왼쪽거만 있을테니 조건문이 undefined일 때는 왼쪽걸 선택하도록 주의해야함.
+
+      if (this.heap[cIdx] > this.heap[targetIndex]) {
+        this.swap(cIdx, targetIndex);
+        cIdx = targetIndex;
+      } else break;
+    }
   }
 }
 
-console.log(...answer);
+arr.forEach((array) => {
+  const pq = new MinHeap();
+  let price = 0;
+  array.forEach((el) => pq.push(el));
+
+  while (pq.length > 1) {
+    let sum = pq.delete() + pq.delete();
+    price += sum;
+    pq.push(sum);
+  }
+
+  console.log(price);
+});
--- 오답노트/3. heap.js
+++ 오답노트/3. heap.js
@@ -1,10 +1,12 @@
 class MaxHeap {
   constructor() {
     this.heap = [];
+    this.length = 0;
   }
 
   push(value) {
     this.heap.push(value);
+    this.length++;
     this.heapifyUp();
   }
 
@@ -12,9 +14,12 @@ class MaxHeap {
     const root = this.heap[0];
 
     if (this.heap.length === 0) return null;
-    else if (this.heap.length === 1) this.heap = [];
-    else {
+    else if (this.heap.length === 1) {
+      this.heap = [];
+      this.length = 0;
+    } else {
       this.heap[0] = this.heap.pop();
+      this.length--;
       this.heapifyDown();
     }
 
@@ -55,6 +60,75 @@ class MaxHeap {
       // 자식노드가 하나만 있을 때에는 왼쪽거만 있을테니 조건문이 undefined일 때는 왼쪽걸 선택하도록 주의해야함.
 
       if (this.heap[cIdx] < this.heap[targetIndex]) {
+        this.swap(cIdx, targetIndex);
+        cIdx = targetIndex;
+      } else break;
+    }
+  }
+}
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+    this.length = 0;
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.length++;
+    this.heapifyUp();
+  }
+
+  delete() {
+    const root = this.heap[0];
+
+    if (this.heap.length === 0) return null;
+    else if (this.heap.length === 1) {
+      this.heap = [];
+      this.length = 0;
+    } else {
+      this.heap[0] = this.heap.pop();
+      this.length--;
+      this.heapifyDown();
+    }
+
+    return root;
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
+  }
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+  swap(index1, index2) {
+    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+  }
+  heapifyUp() {
+    let cIdx = this.heap.length - 1;
+
+    while (cIdx > 0) {
+      const parentIdx = this.getParentIndex(cIdx);
+
+      if (this.heap[cIdx] < this.heap[parentIdx]) {
+        this.swap(cIdx, parentIdx);
+        cIdx = parentIdx;
+      } else break;
+    }
+  }
+  heapifyDown() {
+    let cIdx = 0;
+
+    while (this.getLeftChildIndex(cIdx) < this.heap.length) {
+      const leftChildIndex = this.getLeftChildIndex(cIdx);
+      const rightChildIndex = this.getRightChildIndex(cIdx);
+      const targetIndex = this.heap[leftChildIndex] > this.heap[rightChildIndex] ? rightChildIndex : leftChildIndex;
+      // 자식노드가 하나만 있을 때에는 왼쪽거만 있을테니 조건문이 undefined일 때는 왼쪽걸 선택하도록 주의해야함.
+
+      if (this.heap[cIdx] > this.heap[targetIndex]) {
         this.swap(cIdx, targetIndex);
         cIdx = targetIndex;
       } else break;
