bd1d7517c5a6c68ec6f696cb9f099180fd087c7a
Namgung Jong Mim <jmnamgung@gmail.com>
2024-09-04 14:39:00 +0900

    [Gold V] Title: 평범한 배낭, Time: 216 ms, Memory: 10608 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const [N, K] = input[0].split(" ").map(Number);
+const arr = input.slice(1).map((el) => el.split(" ").map(Number));
+const dp = Array.from({ length: K + 1 }, () => 0);
+
+for (let i = 0; i < N; i++) {
+  for (let j = K; j >= arr[i][0]; j--) {
+    dp[j] = Math.max(dp[j - arr[i][0]] + arr[i][1], dp[j]);
+  }
+}
+
+console.log(dp.at(-1));
