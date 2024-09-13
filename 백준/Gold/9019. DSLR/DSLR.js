25dfc5d350db34b308af6d9a606d3506ff238d11
Namgung Jong Mim <jmnamgung@gmail.com>
2024-09-13 13:45:57 +0900

    [Gold IV] Title: DSLR, Time: 15140 ms, Memory: 47160 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const test = input.slice(1).map((el) => el.split(" ").map(Number));
+let answer = [];
+
+const bfs = (before, after) => {
+  const q = [[before, ""]];
+  const check = { [before]: 1 };
+
+  while (q.length) {
+    const [number, command] = q.shift();
+
+    if (number === after) {
+      answer.push(command);
+      return;
+    }
+
+    // D
+    const n1 = (number * 2) % 10000;
+    if (!check[n1]) {
+      q.push([n1, command + "D"]);
+      check[n1] = 1;
+    }
+
+    // S
+    const n2 = number === 0 ? 9999 : number - 1;
+    if (!check[n2]) {
+      q.push([n2, command + "S"]);
+      check[n2] = 1;
+    }
+
+    // L
+    const n3 = (number % 1000) * 10 + Math.floor(number / 1000);
+    if (!check[n3]) {
+      q.push([n3, command + "L"]);
+      check[n3] = 1;
+    }
+
+    // R
+    const n4 = (number % 10) * 1000 + Math.floor(number / 10);
+    if (!check[n4]) {
+      q.push([n4, command + "R"]);
+      check[n4] = 1;
+    }
+  }
+};
+
+for (let i = 0; i < test.length; i++) {
+  bfs(test[i][0], test[i][1]);
+}
+
+console.log(answer.join("\n"));
